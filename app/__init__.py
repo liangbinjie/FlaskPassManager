08e2f0ec3fbe791494e57e576d2a7835b0e9596d
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-07-08 21:32:49 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-def create_app():
+# def create_app():
     
-    from .main import main_bp
-    app.register_blueprint(main_bp)
+#     from .main import main_bp
+#     app.register_blueprint(main_bp)
     
     
+#     app.run()
+
+from .main import main_bp
+app.register_blueprint(main_bp)
+
+if __name__ == '__main__':
     app.run()
