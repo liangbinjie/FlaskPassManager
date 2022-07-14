7162ae7a19a2a2c6614c6f75b5e466330eaf178e
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-07-14 00:20:16 -0600

    Add functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import *
 from flask_login import *
+from .models import Passwords
+from . import db
 
 main = Blueprint('main', __name__)
 
@@ -7,10 +9,26 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 @login_required
 def index():
-    return render_template('home.html')
+    user_pass = current_user.passwords
+    return render_template('home.html', passwords=user_pass)
 
 
-@main.route("/user")
-def user():
-    return "User"
+@main.route("/add")
+def add():
+    return render_template('config/add.html')
+
+
+@main.route("/add", methods=['POST'])
+def add_post():
+    title = request.form['title']
+    password = request.form['password']
+    url = request.form['url']
+    username = request.form['username']
+    email = request.form['email']
+
+    new_pass = Passwords(title=title, password=password, url=url, email=email, username=username, user_id=current_user.id)
+    db.session.add(new_pass)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
 
